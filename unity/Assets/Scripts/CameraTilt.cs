e39b80e2a22134b8933b98997d3a9394eda577b7
Carl Hildebrandt <hildebrandt.carl@outlook.com>
2020-08-13 17:03:48 -0400

    Added the ability to walk and run in unity. Added the ability to crouch in unity. Added a camera tilt feature in unity

---
+++ unity/Assets/Scripts/CameraTilt.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTilt : MonoBehaviour
+{
+
+    // Update is called once per frame
+    void Update()
+    {
+        float yaw = transform.rotation.eulerAngles.y ;
+        Quaternion target = Quaternion.Euler(0, yaw, 0) ;
+        transform.rotation = target ;
+    }
+}
--- unity/Assets/Scripts/PlayerController.cs
+++ unity/Assets/Scripts/PlayerController.cs
@@ -2,22 +2,37 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+ public enum State
+ {
+    walking, running
+ }
+
 public class PlayerController : MonoBehaviour
 {
-    [HideInInspector]
-    public Animator anim;
-    // public GameObject personObject;
+    [Range(0,60)]
+    public int startDelay;
+    public bool crouched = false;
 
+    public bool loop = false;
+    public State actionType ;
+
+    private Animator anim;
     private int _state = -1;
+    private int _prev_state = -2;
+
     private bool _turning = false;
-    private double start_time;
+    private float _distance_forward = 0;
+    private Vector3 _position;
+
+    private double _start_time;
+    
 
     void Start()
     {
         // Get the animator from this frame
         anim = GetComponent<Animator>();
         // Get the start time
-        start_time = Time.time;
+        _start_time = Time.time;
     }
 
     // Update is called once per frame
@@ -26,71 +41,205 @@ public class PlayerController : MonoBehaviour
         AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(0);
         AnimatorTransitionInfo transInfo = anim.GetAnimatorTransitionInfo(0);
 
-        // Wait 15 seconds before starting
-        if ((_state == -1) && (Time.time - start_time > 15))
+        // Wait to start
+        if (Time.time - _start_time > startDelay)
         {
-            _state = 0;
-        }
-
-        if (_state == 0)
-        {
-            anim.SetFloat("forward", 0.4f);
-        }
-        
-        if ((_state == 0) && (transform.position.z > 8))
-        {
-            anim.SetFloat("forward", 0.0f);
-            anim.SetBool("turn_right", true);
-            _state = _state + 1;
-            _turning = true;
-        }
-
-        if (_state == 2)
-        {
-            anim.SetFloat("forward", 0.65f);
-        }
-
-        if ((_state == 2) && (transform.position.x > 6))
-        {
-            anim.SetFloat("forward", 0.0f);
-            if (stateInfo.IsName("Stand"))
+            if (actionType == State.walking)
             {
-                anim.SetBool("turn_left", true);
-                _state = _state + 1;
-                _turning = true;
+                update_walk(stateInfo, transInfo);
+            }
+
+            if (actionType == State.running)
+            {
+                update_run(stateInfo, transInfo);
+            }
+
+            // Check if we had any turning animation started
+            if (_turning)
+            {
+                checkTurns(transInfo);
+            }
+
+            // Check if we have reached our destination forward
+            if (_distance_forward > 0)
+            {
+                checkForward(stateInfo);
             }
         }
+    }
 
-        if (_state == 4)
+    // Used for walking
+    void update_walk(AnimatorStateInfo stateInfo, AnimatorTransitionInfo transInfo)
+    {
+        // If we have a new state
+        if (_state != _prev_state)
         {
-            if (stateInfo.IsName("Stand"))
+            // Remember that we have dealt with the new state
+            _prev_state = _state;
+            // Process the new state
+            switch(_state)
             {
-                anim.SetBool("turn_left", true);
-                _state = _state + 1;
-                _turning = true;
+                case -1:
+                    anim.SetBool("crouch", crouched);
+                    _state = 0;
+                    break;
+                case 0:
+                    anim.SetFloat("forward", 0.4f);
+                    _distance_forward = 3;
+                    _position = transform.position;
+                    break;
+                case 1:
+                    anim.SetFloat("forward", 0.0f);
+                    anim.SetBool("turn_right", true);
+                    _turning = true;
+                    break;
+                case 2:
+                    anim.SetFloat("forward", 0.4f);
+                    _distance_forward = 6;
+                    _position = transform.position;
+                    break;
+                case 3:
+                    anim.SetFloat("forward", 0.0f);
+                    anim.SetBool("turn_right", true);
+                    _turning = true;
+                    break;
+                case 4:
+                    anim.SetFloat("forward", 0.4f);
+                    _distance_forward = 16;
+                    _position = transform.position;
+                    break;
+                case 5:
+                    anim.SetFloat("forward", 0.0f);
+                    anim.SetBool("turn_right", true);
+                    _turning = true;
+                    break;
+                case 6:
+                    anim.SetFloat("forward", 0.4f);
+                    _distance_forward = 12;
+                    _position = transform.position;
+                    break;
+                case 7:
+                    anim.SetFloat("forward", 0.0f);
+                    anim.SetBool("turn_right", true);
+                    _turning = true;
+                    break;
+                case 8:
+                    anim.SetFloat("forward", 0.4f);
+                    _distance_forward = 13;
+                    _position = transform.position;
+                    break;
+                case 9:
+                    anim.SetFloat("forward", 0.0f);
+                    anim.SetBool("turn_right", true);
+                    _turning = true;
+                    break;
+                case 10:
+                    anim.SetFloat("forward", 0.4f);
+                    _distance_forward = 6;
+                    _position = transform.position;
+                    break;
+                case 11:
+                    anim.SetFloat("forward", 0.0f);
+                    anim.SetBool("turn_left", true);
+                    _turning = true;
+                    break;
+                case 12:
+                    if (loop)
+                    {
+                        _state = -1;
+                    }
+                    break;
             }
         }
+    }
 
-        if (_state == 6)
+    // Used for running
+    void update_run(AnimatorStateInfo stateInfo, AnimatorTransitionInfo transInfo)
+    {
+        // If we have a new state
+        if (_state != _prev_state)
         {
-            anim.SetFloat("forward", 0.65f);
-        }
+            // Remember that we have dealt with the new state
+            _prev_state = _state;
+            // Process the new state
+            switch(_state)
+            {
+                case -1:
+                    _state = 0;
+                    anim.SetBool("crouch", crouched);
+                    break;
+                case 0:
+                    anim.SetFloat("forward", 0.4f);
+                    _distance_forward = 3;
+                    _position = transform.position;
+                    break;
+                case 1:
+                    anim.SetFloat("forward", 0.0f);
+                    anim.SetBool("turn_right", true);
+                    _turning = true;
+                    break;
+                case 2:
+                    anim.SetFloat("forward", 0.4f);
+                    _distance_forward = 6;
+                    _position = transform.position;
+                    break;
+                case 3:
+                    Debug.Log("First turn left");
+                    anim.SetFloat("forward", 0.0f);
+                    anim.SetBool("turn_left", true);
+                    _turning = true;
+                    break;
+                case 4:
+                    if(stateInfo.IsName("Stand") || stateInfo.IsName("Crouch"))
+                    {
+                        _state = _state + 1;
+                    }    
+                    else
+                    {
+                        _prev_state = _state - 1;
+                    }
+                    break;
+                case 5:
+                    Debug.Log("Second turn left");
+                    anim.SetFloat("forward", 0.0f);
+                    anim.SetBool("turn_left", true);
+                    _turning = true;
+                    break;
+                case 6:
+                    Debug.Log("Running");
+                    anim.SetFloat("forward", 0.65f);
+                    _distance_forward = 6;
+                    _position = transform.position;
+                    break;
+                case 7:
+                    anim.SetFloat("forward", 0.0f);
+                    anim.SetBool("turn_left", true);
+                    _turning = true;
+                    break;
+                case 8:
+                    anim.SetFloat("forward", 0.4f);
+                    _distance_forward = 3;
+                    _position = transform.position;
+                    break;
+                case 9:
+                    if (loop)
+                    {
+                        _state = -1;
+                    }
+                    break;
+            }
+        }        
+    }
 
-        if ((_state == 6) && (transform.position.x < 0))
-        {
-            anim.SetFloat("forward", 0.35f);
-            _state = _state + 1;
-        }
-
-        if ((_state == 7) && (transform.position.x < -2))
-        {
-            anim.SetFloat("forward", 0.0f);
-            anim.SetBool("crouch", true);
-        }
+    void checkTurns(AnimatorTransitionInfo transInfo)
+    {
+        bool left_to_stand   = (transInfo.nameHash == Animator.StringToHash("TurnLeft -> Stand"));
+        bool left_to_crouch  = (transInfo.nameHash == Animator.StringToHash("CrouchTurnLeft -> Crouch"));
+        bool right_to_stand  = (transInfo.nameHash == Animator.StringToHash("TurnRight -> Stand"));
+        bool right_to_crouch = (transInfo.nameHash == Animator.StringToHash("CrouchTurnRight -> Crouch"));
 
         // Check if we have finished the rotation
-        
-        if(transInfo.nameHash == Animator.StringToHash("Turn Left -> Stand"))
+        if ((left_to_stand) || (left_to_crouch))
         {
             anim.SetBool("turn_left", false);
             if (_turning == true)
@@ -99,14 +248,45 @@ public class PlayerController : MonoBehaviour
                 _state = _state + 1;
             }
         }
-        if(transInfo.nameHash == Animator.StringToHash("Turn Right -> Stand"))
+        if ((right_to_stand) || (right_to_crouch))
         {
             anim.SetBool("turn_right", false);
             if (_turning == true)
             {
                 _turning = false;
                 _state = _state + 1;
+                fixRotation();
             }
         }
+    }
+
+    void checkForward(AnimatorStateInfo stateInfo)
+    {
+        float stopDelay = 0.0f;
+        if (stateInfo.IsName("Walk"))
+        {
+            stopDelay = 0.65f;
+        }
+        if (stateInfo.IsName("Jog"))
+        {
+            stopDelay = 1f;
+        }
+        
+        float dist = Vector3.Distance(_position, transform.position);
+
+        if (dist > (_distance_forward - stopDelay))
+        {
+            _distance_forward = 0;
+            _state = _state + 1;
+        }
+    }
+
+    void fixRotation()
+    {
+        float x = transform.rotation.eulerAngles.x ;
+        float y = transform.rotation.eulerAngles.y ;
+        float z = transform.rotation.eulerAngles.z ;
+        Quaternion target = Quaternion.Euler(Mathf.Round(x), Mathf.Round(y), Mathf.Round(z)) ;
+        transform.rotation = target ;
     }
 }
